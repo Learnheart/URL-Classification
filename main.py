969f1776d62aefb70d8a9d191006e24479b7a03f
LinhKhanh <110548307+Learnheart@users.noreply.github.com>
2024-05-03 15:35:22 +0700

    display videos

--- main.py
+++ main.py
@@ -7,17 +7,20 @@ from sklearn.feature_extraction.text import CountVectorizer
 from pyvi import ViUtils
 import nltk
 from sklearn.model_selection import train_test_split
-
 nltk.download('stopwords')
 from nltk.stem.snowball import SnowballStemmer
-import numpy as np
-from collections import Counter
 from keras_preprocessing.text import Tokenizer
 from keras.models import load_model
 import numpy as np
-from sklearn.metrics import accuracy_score
 from scipy.stats import mode
 from sklearn.preprocessing import LabelEncoder
+import random
+
+primaryColor="#f63366"
+backgroundColor="#ffffff"
+secondaryBackgroundColor="#f0f2f6"
+textColor="#262730"
+font="sans serif"
 
 names=['URL','Category']
 df=pd.read_csv('full training data.csv',names=names, usecols=[0, 2], na_filter=False,  encoding='latin-1')
@@ -37,11 +40,33 @@ categories = ['Automotive', 'Books & Literature', 'Business & Finance',
        'Home & Garden', 'News & Politics', 'Real Estate', 'Science & Technology',
        'Sports', 'Style & Fashion', 'Travel', 'Games', 'Laws & Policies', 'Environment' ]
 
+gif_paths = {
+    'Automotive': ['media/automotive/car1.gif', 'media/automotive/demo1.jpg'],
+    'Books & Literature': ['media/book&literature/book1gif'],
+    'Business & Finance': ['media/business/business1.gif'],
+    'Careers': ['media/career/career1.gif'],
+    'Education': ['media/career/edu1.jpg'],
+    'Entertainment & Art': ['media/entertainment/demo1.jpg'],
+    'Family & Relationships': ['media/family/family1.gif'],
+    'Food & Drink': ['media/food/food1.gif'],
+    'Healthy Living': ['media/healthy/healthy1.gif'],
+    'Home & Garden': ['media/home/home1.gif'],
+    'News & Politics': ['media/news/news1.gif'],
+    'Real Estate': ['media/real/real1.gif'],
+    'Science & Technology': ['media/science/science1.gif'],
+    'Sports': ['media/sport/sport1.jpg'],
+    'Style & Fashion': ['media/style/style1.gif'],
+    'Travel': ['media/travel/travel1.jpg', 'media/travel/travel2.gif'],
+    'Games': ['media/game/game1.gif'],
+    'Laws & Policies': ['media/law/law1.gif'],
+    'Environment': ['media/environment/environment1.gif'],
+}
+
 xgb_model = joblib.load('xgboost_1.4.1.pkl')
 rf_model = joblib.load('random_forest_1.4.1.pkl')
 dt_model = joblib.load('decision_tree_1.4.1.pkl')
 logis_model = joblib.load('logistic_regression.pkl')
-cnn_model = load_model('cnn_model_16_191.h5')
+cnn_model = load_model('cnn_hyper_2_16.h5')
 svm_model = joblib.load('origin_svm_1.4.1.pkl')
 
 tokenizer = Tokenizer(oov_token='<OOV>')
@@ -88,7 +113,6 @@ def main():
             cnn_predictions = (cnn_predictions > 0.5).astype(int)
             predicted_labels = np.argmax(cnn_predictions, axis=1)
 
-            # Stack all predictions into a NumPy array
             predictions = np.column_stack((rf_predictions, xgboost_predictions, svm_predictions, dt_predictions,
                                            logistic_predictions, predicted_labels))
 
@@ -100,6 +124,14 @@ def main():
             final_predictions_decoded = label_encoder.inverse_transform(final_predictions)
 
             st.success(final_predictions_decoded)
+
+            category = final_predictions_decoded[0]
+            if category in gif_paths and gif_paths[category]:
+                random.shuffle(gif_paths[category])
+                gif_path = gif_paths[category][0]
+                st.image(gif_path, caption=category, use_column_width=True)
+            else:
+                st.warning("No GIF available for this category")
         else:
             st.warning("Please enter a URL")
 
